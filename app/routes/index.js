fe8c57874f51f98107f844464b41dee390a21131
tr.kiyamov <tr.kiyamov@tensor.ru>
2022-03-24 20:47:48 +0300

    create controllers

--- app/routes/index.js
+++
@@ -1,5 +0,0 @@
-const profileRoutes = require('./profile_routes');
-module.exports = function (app, db) {
-  profileRoutes(app, db);
-  // Тут, позже, будут и другие обработчики маршрутов
-};
--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
-// server.js
-const port = 8000;
+//CONFIG
+const dotenv = require('dotenv');
+dotenv.config();
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-require('./app/routes')(app, {});
+const port = process.env.PORT;
+
+//Import routes
+let apiRoutes = require('./routes');
+app.use('/api', apiRoutes);
+
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
+
+const dbPath = process.env.DB_CONNECT_URL;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const mongo = mongoose.connect(dbPath, options);
+mongo.then(
+  () => {
+    console.log('connected');
+  },
+  (error) => {
+    console.log(error, 'error');
+  }
+);
