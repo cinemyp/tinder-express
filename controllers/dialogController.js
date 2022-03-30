54f7572ffcaaa5ff91abc429f7629bd481a0050b
tr.kiyamov <tr.kiyamov@tensor.ru>
2022-03-30 21:24:13 +0300

    переделал концепцию диалогов, чтобы уменьшить количество вдвое

--- controllers/dialogController.js
+++ controllers/dialogController.js
@@ -8,25 +8,31 @@ exports.add = (req, res) => {
 };
 
 exports.view = (req, res) => {
-  Dialog.find({ fromId: req.params.fromId }, async (error, docs) => {
-    if (error) {
-      console.log(error);
-      return res.json({ status: false, error });
-    }
+  Dialog.find(
+    { $or: [{ fromId: req.params.fromId }, { toId: req.params.fromId }] },
+    async (error, docs) => {
+      if (error) {
+        console.log(error);
+        return res.json({ status: false, error });
+      }
 
-    let results = [];
-    for (let doc of docs) {
-      const participant = await Profile.findById(doc.toId).exec();
-      const dialog = {
-        _id: doc._id,
-        toId: doc.toId,
-        fromId: doc.fromId,
-        participant,
-      };
-      results = [...results, dialog];
+      let results = [];
+      for (let doc of docs) {
+        const participantId =
+          req.params.fromId === doc.fromId ? doc.toId : doc.fromId;
+        const participant = await Profile.findById(participantId).exec();
+
+        const dialog = {
+          _id: doc._id,
+          toId: doc.toId,
+          fromId: doc.fromId,
+          participant,
+        };
+        results = [...results, dialog];
+      }
+      res.json(results);
     }
-    res.json(results);
-  }).limit(50);
+  ).limit(50);
 };
 
 exports.createDialog = (toId, fromId, res) => {
@@ -36,17 +42,6 @@ exports.createDialog = (toId, fromId, res) => {
   });
 
   dialog.save((err) => {
-    if (err) {
-      return res.json(err);
-    }
-  });
-
-  const otherDialog = new Dialog({
-    toId: fromId,
-    fromId: toId,
-  });
-
-  otherDialog.save((err) => {
     if (err) {
       return res.json(err);
     }
